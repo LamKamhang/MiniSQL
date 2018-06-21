21b54a04d0bb560367c9de8b192721701527f8b8
LamKamhang <Cool_Lam@outlook.com>
2018-06-21 23:21:48 +0800

    add CatalogManager, by Xue

--- src/CatalogManager/CatalogManager.h
+++ src/CatalogManager/CatalogManager.h
@@ -1,6 +1,9 @@
 #pragma once
 #include<string>
 #include<vector>
+#include<cassert>
+#include<fstream>
+#include<iostream>
 using namespace std;
 
 enum TYPE
@@ -10,27 +13,20 @@ enum TYPE
 	FLOAT
 };
 
-class value
-{
-public:
-	vector<string> values;
-};
-
 class index
 {
 public:
 	string indexName;
 	string tableName;
 	string attributeName;
-	int col;
 	index(string s1, string s2, string s3)
 	{
 		indexName = s1;
 		tableName = s2;
 		attributeName = s3;
 	}
-	index(){}
-		
+	index() {}
+
 };
 
 class attribute
@@ -42,7 +38,12 @@ public:
 	bool primary;
 	bool unique;
 	bool index;
-	attribute(){}
+	attribute() 
+	{
+		primary = false;
+		unique = false;
+		index = false;
+	}
 	~attribute() {}
 	attribute(string n, int t, int l)
 	{
@@ -54,6 +55,7 @@ public:
 		index = false;
 	}
 };
+
 class table
 {
 public:
@@ -61,36 +63,61 @@ public:
 	int attributeNum;
 	int valueNum;
 	vector<attribute> attributes;
-	table(){}
+	table() {}
 	table(string tN, int aN)
 	{
 		tableName = tN;
 		attributeNum = aN;
 		valueNum = 0;
 	}
-	
+
 };
 
-class CatalogManage
+class miniCreateTable
+{
+public:
+	miniCreateTable() {}
+	~miniCreateTable() {}
+	string tableName;
+	int attributeNum;
+	attribute attributes[32];
+};
+
+class miniCreateIndex
+{
+public:
+	string indexName;
+	string tableName;
+	string attributeName;
+};
+
+class CatalogManager
 {
 public:
 	int getTableNum();
 	int getIndexNum();
+	bool readTableFile();
+	bool writeTableFile();
 	bool isTable(string tableName);
-	table getTable(string tableName);
-	bool isAttribute(string tableName, string AN);
+	bool isAttribute(string tableName, string attributeName);
+	bool createTable(miniCreateTable I);
+	bool dropTable(string tableName);
+	bool readIndexFile();
+	bool writeIndexFile();
 	bool isIndex(string indexName);
 	bool isIndex(string tableName, string attributeName);
-	void createTable(table& newTable);
-	index getIndex(string tableName, string attributeName);
-	void dropTable(string tableName);
-	void createIndex(index& newIndex);
-	void dropIndex(string indexName);
-	CatalogManage();
-	~CatalogManage();
+	string getIndex(string tableName, string attributeName);
+	bool createIndex(miniCreateIndex I);
+	bool dropIndex(string indexName);
+	bool dropDatabase();
+	CatalogManager();
+	~CatalogManager();
+
 private:
 	vector<table> tables;
 	vector<index> indices;
 	int tableNum;
 	int indexNum;
 };
+
+int  splitString(const string & strSrc, const std::string& strDelims, vector<string>& strDest);
