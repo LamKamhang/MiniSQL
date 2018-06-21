b53db3a28f0f24323845623696aa39661b150e04
LamKamhang <Cool_Lam@outlook.com>
2018-06-21 23:23:19 +0800

    add IndexManager, fix some API

--- src/IndexManager/IndexManger.cpp
+++ src/IndexManager/IndexManger.cpp
@@ -1,56 +1,118 @@
-#include "IndexManager.h"
+/*
+ * File: IndexManager.cpp
+ * Version: 1.1
+ * Author: kk
+ * Created Date: Sat Jun 16 01:07:47 DST 2018
+ * Modified Date: Thu Jun 21 21:06:52 DST 2018
+ * -------------------------------------------
+ * miniSQL的IndexManager的类函数实现
+ * version 1.0: 基本操作的实现
+ * version 1.1: 修改了select 以及 delete的接口，使得能够判断异常状态
+ */
 
-IndexManager::IndexManager()
+IndexManager::IndexManager(BufferManager& bm)
 {
+    intBPTree = new BPTree<int>(bm);
+    floatBPTree = new BPTree<float>(bm);
+    stringBPTree = new BPTree<std::string>(bm);
 }
 
 IndexManager::~IndexManager()
 {
+    if(intBPTree->isChanged())
+        intBPTree->wiriteBack();
+    if(floatBPTree->isChanged())
+        floatBPTree->wiriteBack();
+    if(stringBPTree->isChanged())
+        stringBPTree->wiriteBack();
+   
+    delete intBPTree;
+    delete floatBPTree;
+    delete stringBPTree;
 }
 
-// select
-std::vector<TuplePtr> IndexManager::_select(const std::string &indexName, const condition &cond)
+/*
+class condition
 {
-    switch(cond.type)
-    {
-        case INT:       return int_select(indexName, cond);
-        case FLOAT:     return float_select(indexName, cond);
-        case CHAR:      return string_select(indexName, cond);
-        default:        return std::vector<TuplePtr>{};
-    }
-}
+public:
+	string attributeName;
+	int type;
+	string oprt;
+	int intValue;
+	float floatValue;
+	string stringValues;
+};
 
-std::vector<TuplePtr> IndexManager::int_select(const std::string &indexName, const condition &cond)
+// 属性的类型/值的类型
+enum AttributeType {
+    None,
+    Integer,
+    Float, 
+    String,
+};
+typedef AttributeType SqlValueType;
+
+// 操作符的类型
+enum Operator{
+    ERROR,
+    EQ,
+    NE,
+    LE,
+    GE,
+    LT,
+    GT,
+};
+*/
+bool IndexManager::_select(const std::string &indexName, const condition &cond, std::vector<TuplePtr> &tuplePtrs)
 {
-    if (intTrees.find(indexName) == intTrees.end())
+    tuplePtrs.resize(0);
+    GetBPTree(indexName, type);
+    switch(cond.oprt)
     {
-        intTrees[indexName] = BPTree(indexName, DEGREE);
+        case EQ:
+            switch(type)
+            {
+                case Integer:   tuplePtrs.push_back(intBPTree->_search(cond.intValue));    break;
+                case Float:     tuplePtrs.push_back(floatBPTree->_search(cond.floatValue));  break;
+                case String:    tuplePtrs.push_back(stringBPTree->_search(cond.stringValues)); break;
+            }
+            break;
+        case LE:
+        case GE:
+        case LT:
+        case GT:
+        case NE:
+        default: return false;
     }
 
-    switch (cond.op)
-    {
-        case EQ:    return std::vector<TuplePtr>{
-                        intTrees[indexName]->find(cond.intValue)
-                    };
-        case LE:    return intTrees[indexName]->findRange(cond.intValue, LE);
-        case GE:    return intTrees[indexName]->findRange(cond.intValue, GE);
-        case LT:    return intTrees[indexName]->findRange(cond.intValue, LT);
-        case GT:    return intTrees[indexName]->findRange(cond.intValue, GT);
-        case NE:    return intTrees[indexName]->findRange(cond.intValue, NE);
-        default:    return std::vector<TuplePtr>{};
-    }
+    return true;
 }
 
-std::vector<TuplePtr> IndexManager::_delete(const std::string &indexName, const condition &cond)
+bool IndexManager::_delete(const std::string &indexName, const condition &cond, std::vector<TuplePtr> &tuplePtrs)
 {
-    switch(cond.type)
+    bool canDo = _select(indexName, condition, tuplePtrs);
+    switch(cond.oprt)
     {
-        case INT:       return int_delete(indexName, cond);
-        case FLOAT:     return float_delete(indexName, cond);
-        case CHAR:      return string_delete(indexName, cond);
-        default:        return std::vector<TuplePtr>{};
+        case EQ:
+            switch(type)
+            {
+                case Integer:   intBPTree->_delete(cond.intValue);    break;
+                case Float:     floatBPTree->_delete(cond.floatValue);  break;
+                case String:     stringBPTree->_delete(cond.stringValues);break;
+            }
+            break;
+        case LE:
+        case GE:
+        case LT:
+        case GT:
+        case NE:
+        default: return false;
     }
+    return true;
 }
+
+// update <table_name> set <attribute> = <new_value> where <conditions>
+// bool _update(const std::string &indexName, const condition &oldValue, const condition &newValue, const TuplePtr &newTuplePtr);
 
 /*
 class condition
@@ -66,37 +128,113 @@ public:
 */
 bool IndexManager::_insert(const std::string &indexName, const condition &value, const TuplePtr &valueptr)
 {
+    GetBPTree(indexName, value.type);
     switch(value.type)
     {
-        case INT:   return int_insert(indexName, value, valueptr);
-        case FLOAT: return float_insert(indexName, value, valueptr);
-        case CHAR:  return string_insert(indexName, value, valueptr);
-        default:    return false;
+        case Integer:   intBPTree->_insert(value.intValue, valueptr);    break;
+        case Float:     floatBPTree->_insert(value.floatValue, valueptr);    break;
+        case String:    stringBPTree->_insert(value.stringValues, valueptr); break;
+        default: return false;
     }
+    return true;
 }
 
+/*
+class records
+{
+public:
+	int attriNum;//属性的个数
+	vector<attribute> attributes;//属性信息
+	int recordNum;//记录的个数
+	vector<miniRecord> list;//记录的数据
+}
+
+class miniRecord
+{
+public:
+	condition cond[32];//记录内容 
+	int conditionNum;//属性数目 
+	int pos;//所在块的偏移 
+	int blockNum;//所在块的编号 
+}
+*/
 bool IndexManager::_create(const std::string &indexName, const records &values, int index)
 {
-    switch(values.attributes[index].type)
+    FILE *fp = fopen(indexName.c_str(), "r");
+    if (fp != NULL)
     {
-        case INT:       return int_create(indexName, values.list, index);
-        case FLOAT:     return float_create(indexName, values.list, index);
-        case CHAR:      return string_create(indexName, values.list, index);
-        default:        return false;
+        std::cerr << "ERROR: INDEX EXIST!" << std::endl;
+        fclose(fp);
+        return false;
     }
+    fclose(fp);
+
+    SqlValueType type = values.attributes[index].type;
+    const std::vector<miniRecord> &_list = values.list;
+    switch(type)
+    {
+        case Integer:   
+            intBPTree->createNewFile(indexName, type);  
+            for (int i = 0; i < values.recordNum; ++i)
+            {
+                intBPTree->_insert(_list[i].cond[index].intValue, TuplePtr((unsigned int)(_list[i].blockNum), (unsigned int)(_list[i].pos)));
+            }
+            break;
+        case Float:     
+            floatBPTree->createNewFile(indexName, type);
+            for (int i = 0; i < values.recordNum; ++i)
+            {
+                floatBPTree->_insert(_list[i].cond[index].floatValue, TuplePtr((unsigned int)(_list[i].blockNum), (unsigned int)(_list[i].pos)));
+            }
+            break;
+        case String:    
+            stringBPTree->createNewFile(indexName, type);
+            for (int i = 0; i < values.recordNum; ++i)
+            {
+                stringBPTree->_insert(_list[i].cond[index].stringValues, TuplePtr((unsigned int)(_list[i].blockNum), (unsigned int)(_list[i].pos)));
+            }
+            break;
+        default: return false;
+    }
+    return true;
 }
 
 bool IndexManager::_drop(const std::string &indexName)
 {
-    if (intTrees.find(indexName) != intTrees.end())
-        intTrees.erase(indexName);
-    else if (floatTrees.find(indexName) != floatTrees.end())
-        floatTrees.erase(indexName);
-    else if (stringTrees.find(indexName) != stringTrees.end())
-        stringTrees.erase(indexName);
-    else
+    // check whether the index exists.
+    FILE* fp = fopen(indexName.c_str(), "r");
+    if (fp == NULL)
     {
+        std::cerr << "ERROR: INDEX FILE " << indexName << "NOT EXIST!" << std::endl;
         return false;
+    }
+    fclose(fp);
+
+    // release any block about the file
+    bm.DeleteFileBlock(indexName);
+    // delete file in the disk!
+    remove(filename.c_str());
+
+    // update BPTree info to avoid rewrite into disk. 
+    if (intBPTree->indexName == indexName)
+        intBPTree->_release();
+    else if(floatBPTree->indexName == indexName)
+        floatBPTree->_release();
+    else if (stringBPTree->indexName == indexName)
+        stringBPTree->_release();
+    
+    return true;
+}
+
+// private function.
+void IndexManager::GetBPTree(std::string indexName, SqlValueType type)
+{  
+    switch(type)
+    {
+        case Integer:   intBPTree->readFromFile(indexName); break;
+        case Float:     floatBPTree->readFromFile(indexName);break;
+        case String:    stringBPTree->readFromFile(indexName);break;
+        default: return false;
     }
     return true;
 }
--- src/IndexManager/IndexManger.h
+++ src/IndexManager/IndexManger.h
@@ -1,45 +1,131 @@
+/*
+ * File: IndexManager.h
+ * Version: 1.1
+ * Author: kk
+ * Created Date: Sat Jun 16 01:07:47 DST 2018
+ * Modified Date: Thu Jun 21 21:06:52 DST 2018
+ * -------------------------------------------
+ * miniSQL的IndexManager的类头文件声明
+ * version 1.0: 基本操作的实现
+ * version 1.1: 修改了select 以及 delete的接口，使得能够判断异常状态
+ */
+
 #pragma once
 #include "../interface.h"
 #include "BPTree.hpp"
+#include "../BufferManager/BufferManager.h"
 
 #include <string>
 #include <vector>
 #include <iostream>
-#include <map>
 
+/*
+ * class: IndexManager
+ * Version: 1.0
+ * Author: kk
+ * Created Date: Sat Jun 16 01:07:47 DST 2018
+ * -------------------------------------------
+ * IndexManager类声明
+ */
 class IndexManager
 {
-public:
-    IndexManager();
+private:
+    // 不允许拷贝构造
     IndexManager(IndexManager &&) = default;
     IndexManager(const IndexManager &) = default;
+public:
+    IndexManager(BufferManager& bm);
     ~IndexManager();
 
-    std::vector<TuplePtr> _select(const std::string &indexName, const condition &cond);
+    /*
+	 * Function: _select
+	 * Argument: first arg: indexName.
+	 *			 second arg: the condition, but just one condition.
+	 *		     third arg: return ptrs for record.
+	 * Version: 1.1
+	 * Author: kk
+	 * Created Date: Sat Jun 16 01:07:47 DST 2018
+	 * Modified Date: Thu Jun 21 21:06:52 DST 2018
+	 * -------------------------------------------
+	 * miniSQL的IndexManager的类函数实现
+	 * version 1.1: 修改为返回值是bool型，将所需返回的TuplePtr放在了最后作为引用变量
+	 */
+    bool _select(const std::string &indexName, const condition &cond, std::vector<TuplePtr> &tuplePtrs);
 
-    std::vector<TuplePtr> _delete(const std::string &indexName, const condition &cond);
+    // 区间搜索的接口定义有点麻烦，暂定
+    //bool range_select(const std::string &indexName, ...)
+    
+    /*
+	 * Function: _delete
+	 * Argument: first arg: indexName.
+	 *			 second arg: the condition, but just one condition.
+	 *		     third arg: return ptrs for record.
+	 * Version: 1.1
+	 * Author: kk
+	 * Created Date: Sat Jun 16 01:07:47 DST 2018
+	 * Modified Date: Thu Jun 21 21:06:52 DST 2018
+	 * -------------------------------------------
+	 * miniSQL的IndexManager的类函数实现
+	 * version 1.1: 修改为返回值是bool型，将所需返回的TuplePtr放在了最后作为引用变量
+	 */
+    bool _delete(const std::string &indexName, const condition &cond, std::vector<TuplePtr> &tuplePtrs);
 
+    // update <table_name> set <attribute> = <new_value> where <conditions>
+    // bool _update(const std::string &indexName, const condition &oldValue, const condition &newValue, const TuplePtr &newTuplePtr);
+
+    /*
+	 * Function: _insert
+	 * Argument: first arg: indexName.
+	 *			 second arg: this is a value(...why not use another type...too strange).
+	 *		     third arg: the tupleptr belongs to the value
+	 * Version: 1.0
+	 * Author: kk
+	 * Created Date: Sat Jun 16 01:07:47 DST 2018
+	 * -------------------------------------------
+	 * miniSQL的IndexManager的类函数实现
+	 */
     bool _insert(const std::string &indexName, const condition &value, const TuplePtr &valueptr);
 
+    /*
+	 * Function: _create
+	 * Argument: first arg: indexName.
+	 *			 second arg: these are the records in the whole table.
+	 *		     third arg: this is the index of the index_attribute.
+	 *			the type records is too complicated... just look up in the cpp... amazing!!!
+	 * Version: 1.0
+	 * Author: kk
+	 * Created Date: Sat Jun 16 01:07:47 DST 2018
+	 * -------------------------------------------
+	 * miniSQL的IndexManager的类函数实现
+	 */
     bool _create(const std::string &indexName, const records &values, int index);
-    
+ 
+    /*
+	 * Function: _drop
+	 * Argument: first arg: indexName.
+	 * Version: 1.0
+	 * Author: kk
+	 * Created Date: Sat Jun 16 01:07:47 DST 2018
+	 * -------------------------------------------
+	 * miniSQL的IndexManager的类函数实现
+	 */   
     bool _drop(const std::string &indexName);
 private:
-    std::vector<TuplePtr> int_select    (const std::string &indexName, const condition &cond);
-    std::vector<TuplePtr> float_select  (const std::string &indexName, const condition &cond);
-    std::vector<TuplePtr> string_select   (const std::string &indexName, const condition &cond);
+    /*
+	 * Function: GetBPTree
+	 * Argument: first arg: indexName.
+	 *			 second arg: the BPtree's type
+	 * Version: 1.0
+	 * Author: kk
+	 * Created Date: Sat Jun 16 01:07:47 DST 2018
+	 * -------------------------------------------
+	 * 内部函数，用来更新im当前的私有成员变量
+	 */ 
+    void GetBPTree(std::string indexName, SqlValueType type);
 
-    std::vector<TuplePtr> int_delete    (const std::string &indexName, const condition &cond);
-    std::vector<TuplePtr> float_delete  (const std::string &indexName, const condition &cond);
-    std::vector<TuplePtr> string_delete   (const std::string &indexName, const condition &cond);    
+    BPTree<int> *intBPTree;
+    BPTree<float> *floatBPTree;
+    BPTree<std::string> *stringBPTree;
 
-    typedef std::map<std::string, BPTree<int> *>            IntTrees;
-    typedef std::map<std::string, BPTree<float> *>          FloatTrees;
-    typedef std::map<std::string, BPTree<std::string> *>    StringTrees;
-
-    IntTrees    intTrees;
-    FloatTrees  floatTrees;
-    StringTrees   stringTrees;
-
-    enum {DEGREE = 200};
+    BufferManager& bm;
 };
